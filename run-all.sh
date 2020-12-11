1035c9fdcba4b57024744346465d6be9d8cac189
yangzhiqiang1 <yangzhiqiang1@cnpc.com.cn>
2020-12-11 16:11:54 +0800

    del exec

--- run-all.sh
+++ run-all.sh
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-exec /usr/sbin/init
+/usr/sbin/init
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
 
